3cadca096bce180ed6721455e2f416a041e039c4
nukoneZ <163789053+kykhongbithac@users.noreply.github.com>
2026-06-22 12:24:11 +0700

    Delete backend_data directory

--- backend_data/src/utils/apiError.js
+++
@@ -1,9 +0,0 @@
-class ApiError extends Error {
-  constructor(statusCode, message, code = 'API_ERROR') {
-    super(message);
-    this.statusCode = statusCode;
-    this.code = code;
-  }
-}
-
-module.exports = ApiError;
